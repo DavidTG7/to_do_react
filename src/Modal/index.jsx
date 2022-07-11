69e638c1ffc79c86f51c1c3ce6c6ca135360f866
DavidTG7 <davidthekoa@gmail.com>
2022-07-11 17:22:15 -0500

    Create Modal Component to render CreateItem concitionaly

---
+++ src/Modal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
+
+
+export const Modal = ( {children } ) => {
+  return ReactDOM.createPortal(
+    <div className='modal-background'>
+      {children}
+    </div>,
+    document.getElementById('modal')
+  )
+}
